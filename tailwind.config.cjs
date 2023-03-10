06f31b0b20a8c1e5508705ed78394ef9222bcfcb
bendigiorgio <bendigiorgio@gmail.com>
2023-03-10 21:18:30 +0900

    New package/fixed theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,25 +3,25 @@ const { fontFamily } = require("tailwindcss/defaultTheme");
 const config = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      primary: "#FDFCFB",
-      "h-yellow": "#F1AF05",
-      "h-orange": "#FF8B1A",
-      "h-red": "#B12522",
-      "s-blue": "#27314C",
-      "s-purple-grey": "#403A47",
-      "s-purple": "#211C30",
-      "s-grey": "#272424",
-    },
     extend: {
       fontFamily: {
         krylon: ["var(--krylon-font)", ...fontFamily.serif],
         josefin: ["var(--josefin-font)", ...fontFamily.serif],
         times: ["Times", ...fontFamily.serif],
       },
+      colors: {
+        primary: "#FDFCFB",
+        "h-yellow": "#F1AF05",
+        "h-orange": "#FF8B1A",
+        "h-red": "#B12522",
+        "s-blue": "#27314C",
+        "s-purple-grey": "#403A47",
+        "s-purple": "#211C30",
+        "s-grey": "#272424",
+      },
     },
   },
-  plugins: [],
+  plugins: [require("tailwind-scrollbar-hide")],
 };
 
 module.exports = config;
